0f47fd16e9b1b35fddac33d1029d0a1ed716b3a5
Hazama25 <hazamafawkes@gmail.com>
2020-08-12 17:38:28 +0800

    lavender: Xtended Bringup
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/du_lavender.mk
-
-COMMON_LUNCH_CHOICES := \
-    du_lavender-user \
-    du_lavender-userdebug \
-    du_lavender-eng
+    $(LOCAL_DIR)/xtended_lavender.mk
--- xtended_lavender.mk
+++ xtended_lavender.mk
@@ -15,12 +15,14 @@ $(call inherit-product, device/xiaomi/lavender/device.mk)
 # Inherit dalvik from native
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
-# Inherit some common Corvus stuff.
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
-$(call inherit-product, device/corvus/CorvusParts/corvusparts.mk)
+# Inherit some common Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+XTENDED_BUILD_TYPE := OFFICIAL
+XTENDED_MAINTAINER := ⚡Hazama25⚡
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := du_lavender
+PRODUCT_NAME := xtended_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -32,7 +34,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 20.3.19 release-keys"
 
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys"
-
-DU_BUILD_TYPE := OFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
