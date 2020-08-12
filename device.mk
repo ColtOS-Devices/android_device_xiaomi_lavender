f8c2a951ba4120ab46b27c313b963dd628ec33a2
AmulyaX <amulya.b520@gmail.com>
2020-08-12 17:14:32 +0800

    lavender: use MIUI poweroff charging animation
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -189,7 +189,8 @@ PRODUCT_PACKAGES += \
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service \
+    chargeonlymode
 
 # HIDL
 PRODUCT_PACKAGES += \
